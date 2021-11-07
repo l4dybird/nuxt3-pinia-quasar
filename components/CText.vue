71fc011be72ecf23288670a6d1452f92fb0d29db
yukimaruDubstep <k.yuuki1997@gmail.com>
2021-11-07 20:44:12 +0900

    :sparkles: add template

---
+++ components/CText.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    {{ props.text }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  text: String
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <CText :text="text" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const text = ref<string>("pages");
+</script>
