ce4a6fda711450a61c7378d6ff6153e2e101b67c
yukimaruDubstep <k.yuuki1997@gmail.com>
2021-11-22 12:31:55 +0900

    :fire:　deleted favicon.png

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    header
     <slot />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,9 @@ import { quasar } from '@quasar/vite-plugin'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     ssr: false,
-    head: {
-        link: [{ href: "/favicon.png" }]
-    },
+    meta: {
+        title: 'Nuxt3 + Pinia + Quasar',
+      },
     css: [
         '~/assets/styles/quasar.sass',
         '@quasar/extras/material-icons/material-icons.css'
