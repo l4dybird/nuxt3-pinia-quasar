ee96ed5dded78b13a0c2f6b63a0c3d4a5049edbe
yukimaruDubstep <k.yuuki1997@gmail.com>
2021-12-31 13:19:42 +0900

    :recycle: spa build settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import { quasar } from '@quasar/vite-plugin'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     ssr: false,
+    target:"static",
     meta: {
         title: 'Nuxt3 + Pinia + Quasar',
     },
