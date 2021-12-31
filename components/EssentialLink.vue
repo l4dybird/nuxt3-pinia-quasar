8fbb373e3688b9e39bd3169239a7fa38f2c213b4
yukimaruDubstep <k.yuuki1997@gmail.com>
2021-12-31 14:53:44 +0900

    :recycle: Change from defineCoponent to script setup

--- components/EssentialLink.vue
+++ components/EssentialLink.vue
@@ -1,3 +1,24 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  caption: {
+    type: String,
+    default: ''
+  },
+  link: {
+    type: String,
+    default: '#'
+  },
+  icon: {
+    type: String,
+    default: ''
+  }
+});
+</script>
+
 <template>
   <q-item
     clickable
@@ -20,32 +41,3 @@
     </q-item-section>
   </q-item>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'EssentialLink',
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-
-    caption: {
-      type: String,
-      default: ''
-    },
-
-    link: {
-      type: String,
-      default: '#'
-    },
-
-    icon: {
-      type: String,
-      default: ''
-    }
-  }
-})
-</script>
