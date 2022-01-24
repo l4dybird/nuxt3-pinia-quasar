041d5caca77b9fe65dae4f5daff2126e2bfdd347
yukimaruDubstep <k.yuuki1997@gmail.com>
2022-01-24 14:26:30 +0900

    :sparkles: 子コンポーネント内にいるバリデーションメソッドを親メソッドで呼び出す実装

---
+++ components/TextField.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+type Props = {
+    value?: string;
+    rules?: any;
+}
+
+defineProps<Props>();
+
+const textFieldRef = ref(null);
+
+defineExpose({
+  textFieldRef,
+});
+</script>
+
+<template>
+  <q-input
+    ref="textFieldRef"
+    filled
+    v-model="value"
+    label="Your text *"
+    hint="Name and surname"
+    lazy-rules
+    :rules="rules"
+  ></q-input>
+</template>
---
+++ pages/validate.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const text = ref(null);
+const textFieldRef = ref(null);
+
+const accept = ref(false);
+
+const textRules = [
+  val => (val && val.length > 0) || 'Please type something'
+];
+
+const onSubmit = () => {
+  textFieldRef.value.textFieldRef.validate();
+
+  if (textFieldRef.value.textFieldRef.hasError) {
+    // form has error
+  }
+};
+
+const onReset = () => {
+  text.value = null;
+  textFieldRef.value.textFieldRef.resetValidation();
+};
+onMounted(() => {
+  console.log(textFieldRef.value);
+})
+</script>
+
+<template>
+  <q-page class="row items-center justify-evenly">
+    <div id="q-app" style="min-height: 100vh;">
+      <div class="q-pa-md" style="max-width: 300px">
+        <form @submit.prevent.stop="onSubmit" @reset.prevent.stop="onReset" class="q-gutter-md">
+          <text-field
+            ref="textFieldRef"
+            v-model="text"
+            :rules="textRules"
+          ></text-field>
+
+          <q-toggle v-model="accept" label="I accept the license and terms"></q-toggle>
+
+          <div>
+            <q-btn label="Submit" type="submit" color="primary"></q-btn>
+            <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm"></q-btn>
+          </div>
+        </form>
+      </div>
+    </div>
+  </q-page>
+</template>
