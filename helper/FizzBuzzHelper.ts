1fc654e44c48fc56e28daf75a82b0b1d5833b9c0
yukimaruDubstep <k.yuuki1997@gmail.com>
2022-01-28 00:05:27 +0900

    :sparkles: add vitest

---
+++ helper/FizzBuzzHelper.ts
@@ -0,0 +1,17 @@
+const FizzBuzzHelper = (value: number): string => {
+    if(typeof value === 'number') {
+        if(value%3 === 0 && value%5 !== 0){
+            return 'Fizz'
+        }else if(value%3 !== 0 && value%5 === 0){
+            return 'Buzz'
+        }else if(value%3 === 0 && value%5 === 0){
+            return 'FizzBuzz'
+        }else{
+            return value.toString();
+        }
+    } else {
+        return 'number type only'
+    }
+}
+
+export default FizzBuzzHelper;
