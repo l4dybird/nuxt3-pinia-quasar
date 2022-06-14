9aee52d20521531ed0d3a8031f2b3cbb7fc3f135
Yuki <k.yuuki1997@gmail.com>
2022-06-14 19:42:07 +0900

    :recycle: Add directives to plugins/quasar

--- plugins/quasar.ts
+++ plugins/quasar.ts
@@ -1,10 +1,11 @@
 import { Quasar } from 'quasar'
-import * as All from 'quasar'
+import * as components from 'quasar'
+import * as directives from 'quasar'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(Quasar, {
     config: {},
-    components: All,
-    plugins: {},
+    components,
+    directives
   })
 });
