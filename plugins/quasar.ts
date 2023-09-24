f6d8493ed911031154948b64e93898850fc11764
l4dybird <future.l4dy61rd@gmail.com>
2023-09-24 21:28:15 +0900

    :package: update package

--- plugins/quasar.ts
+++ plugins/quasar.ts
@@ -1,11 +1,38 @@
 import { Quasar } from "quasar";
 import * as components from "quasar";
-import * as directives from "quasar";
+import {
+  ClosePopup as vClosePopup,
+  Intersection as vIntersection,
+  Morph as vMorph,
+  Mutation as vMutation,
+  Ripple as vRipple,
+  Scroll as vScroll,
+  ScrollFire as vScrollFire,
+  TouchHold as vTouchHold,
+  TouchPan as vTouchPan,
+  TouchRepeat as vTouchRepeat,
+  TouchSwipe as vTouchSwipe,
+} from "quasar";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(Quasar, {
-      config: {},
-      components,
-      directives,
+    components,
+    directives: {
+      vClosePopup,
+      vIntersection,
+      vMorph,
+      vMutation,
+      vRipple,
+      vScroll,
+      vScrollFire,
+      vTouchHold,
+      vTouchPan,
+      vTouchRepeat,
+      vTouchSwipe,
+    },
+    plugins: {},
+    iconSet: undefined,
+    config: {},
+    lang: undefined,
   });
 });
